f200e609023b114fdcb512d48936196187d15690
Shahmeer Arshad <ranashahmeerarshad@gmail.com>
2026-06-24 16:35:15 +0500

    Implement menu toggle and active link highlighting
    Add menu toggle functionality and highlight active link.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
 
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+});
+
+const currentPage = window.location.pathname.split("/").pop();
+
+const links = document.querySelectorAll(".nav-links a");
+
+links.forEach(link => {
+    const href = link.getAttribute("href");
+
+    if (href === currentPage) {
+        link.classList.add("active");
+    }
+});
